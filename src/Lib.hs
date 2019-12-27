6c1287cc62b59ec90c59d4ff40b223ad01bd9d58
Gerardo Munguia <gerardomunguiahoyo@gmail.com>
2019-12-27 16:37:19 +0100

    fetch story ids from api

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE DeriveGeneric  #-}
-{-# LANGUAGE DeriveAnyClass #-}
-
 module Lib where
 
 import Aws.Lambda
@@ -11,41 +8,23 @@ import Network.HTTP.Simple
 import qualified Data.Vector as V
 import qualified Data.Scientific as S
 
-data Person = Person
-  { personName :: String
-  , personAge :: Int
-  } deriving (Generic)
-instance FromJSON Person
-instance ToJSON Person
-
-toNum :: Int -> Value
-toNum n = Number $ S.scientific (toInteger n) 1
-
 data Story = Story { id :: Int } deriving (Show)
 instance ToJSON Story where
-  toJSON (Story id) = toNum id
+  toJSON (Story id) = Number $ S.scientific (toInteger id) 1
+instance FromJSON Story where
+  parseJSON (Number x) = maybe (typeMismatch "Int" $ Number x) (return . Story . Prelude.id) $ S.toBoundedInteger x
+  parseJSON invalid = typeMismatch "Int" invalid
 
 newtype Stories = Stories { stories :: [Story] } deriving (Show)
 instance ToJSON Stories where
   toJSON (Stories stories) = Array $ V.fromList $ toJSON <$> stories
-
-parseI :: Value -> Either String Int
-parseI (Number v) =
-  maybe (Left "Expected integer") (\i -> Right i) $
-  S.toBoundedInteger v
-parseI invalid = Left "Expected number"
-
-parseA :: Value -> Either String [Int]
-parseA (Array v) = (mapM parseI $ V.toList v)
-parseA invalid = Left "Expected an Array"
-
-intsToStories :: [Int] -> Stories
-intsToStories ints = Stories $ Story <$> ints
+instance FromJSON Stories where
+  parseJSON (Array a) = Stories <$> traverse parseJSON (V.toList a)
+  parseJSON invalid = typeMismatch "Array" invalid
 
 handler :: Value -> Context -> IO (Either String Stories)
 handler _ context =
   parseRequest "https://hacker-news.firebaseio.com/v0/newstories.json"
-  >>= (\req -> (httpJSON req) )
-  >>= (\res -> return $ getResponseBody res)
-  >>= (\body -> return $ parseA body)
-  >>= (\parsed -> return $ intsToStories <$> parsed)
+  >>= httpJSON
+  >>= return . getResponseBody
+  >>= return . parseEither parseJSON
