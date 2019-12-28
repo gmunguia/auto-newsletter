bbb8364b43af3156930c2d50ccbd759a862ecc7a
Gerardo Munguia <gerardomunguiahoyo@gmail.com>
2019-12-28 23:17:26 +0100

    add sqs queue

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,18 +1,22 @@
 module Lib where
 
+import System.Environment
 import Aws.Lambda
 import GHC.Generics
 import Data.Aeson
 import Data.Aeson.Types
 import Network.HTTP.Simple
+import qualified Data.Text as T
 import qualified Data.Vector as V
-import qualified Data.Scientific as S
+import qualified Data.Scientific as Sci
+import qualified Network.AWS.SQS as SQS
+import qualified Control.Lens as L
 
-data Story = Story { id :: Int } deriving (Show)
+data Story = Story { storyId :: Int } deriving (Show)
 instance ToJSON Story where
-  toJSON (Story id) = Number $ S.scientific (toInteger id) 1
+  toJSON (Story id) = Number $ Sci.scientific (toInteger id) 1
 instance FromJSON Story where
-  parseJSON (Number x) = maybe (typeMismatch "Int" $ Number x) (return . Story . Prelude.id) $ S.toBoundedInteger x
+  parseJSON (Number x) = maybe (typeMismatch "Int" $ Number x) (return . Story . Prelude.id) $ Sci.toBoundedInteger x
   parseJSON invalid = typeMismatch "Int" invalid
 
 newtype Stories = Stories { stories :: [Story] } deriving (Show)
@@ -22,9 +26,43 @@ instance FromJSON Stories where
   parseJSON (Array a) = Stories <$> traverse parseJSON (V.toList a)
   parseJSON invalid = typeMismatch "Array" invalid
 
-handler :: Value -> Context -> IO (Either String Stories)
+join :: String -> [String] -> String
+join _ [] = ""
+join comma (x:xs) = x ++ comma ++ join comma xs
+
+wrap x = [x]
+unwrap (x:[]) = x
+
+sendBatchToSqs :: Stories -> IO SQS.SendMessageBatch
+sendBatchToSqs (Stories ids) =
+  let
+    storyToEntry = (\s -> let id = (T.pack . show) s in SQS.sendMessageBatchRequestEntry id id) :: Story -> SQS.SendMessageBatchRequestEntry
+    entries = storyToEntry <$> ids :: [SQS.SendMessageBatchRequestEntry]
+  in do
+    url <- getEnv "QUEUE_URL"
+    return $ L.set SQS.smbEntries entries $ SQS.sendMessageBatch (T.pack url)
+
+chunk :: Int -> [a] -> [[a]]
+chunk _ [] = []
+chunk n xs = let (firstN, rest) = splitAt n xs in [firstN] ++ chunk n rest
+
+sendToSqs :: Story -> IO SQS.SendMessage
+sendToSqs (Story id) = do
+  url <- getEnv "QUEUE_URL"
+  return $ SQS.sendMessage (T.pack url) (T.pack $ show id)
+
+sendAllToSqs :: Stories -> IO [SQS.SendMessage]
+sendAllToSqs ss = traverse sendToSqs $ stories ss
+
+ignore :: a -> ()
+ignore _ = ()
+
+handler :: Value -> Context -> IO (Either String ())
 handler _ context =
   parseRequest "https://hacker-news.firebaseio.com/v0/newstories.json"
   >>= httpJSON
   >>= return . getResponseBody
   >>= return . parseEither parseJSON
+  >>= return . fmap (Stories . take 10 . stories)
+  >>= traverse sendAllToSqs
+  >>= return . fmap ignore
--- stack.js
+++ stack.js
@@ -1,3 +1,5 @@
+const sqs = require("@aws-cdk/aws-sqs");
+const { SqsEventSource } = require("@aws-cdk/aws-lambda-event-sources");
 const lambda = require("@aws-cdk/aws-lambda");
 const cdk = require("@aws-cdk/core");
 
@@ -5,19 +7,32 @@ class NewsletterStack extends cdk.Stack {
   constructor(app, id) {
     super(app, id);
 
+    const dlq = new sqs.Queue(this, "news-ids-dlq", {});
+
+    const queue = new sqs.Queue(this, "news-ids-queue", {
+      retentionPeriod: cdk.Duration.seconds(60),
+      deadLetterQueue: {
+        maxReceiveCount: 1,
+        queue: dlq
+      }
+    });
+
     const haskellRuntime = lambda.LayerVersion.fromLayerVersionArn(
       this,
       "HaskellRuntime",
       "arn:aws:lambda:eu-west-1:785355572843:layer:aws-haskell-runtime:2"
     );
 
-    new lambda.Function(this, "test", {
+    const extractNewsLambda = new lambda.Function(this, "test", {
       code: lambda.Code.fromAsset("build/function.zip"),
       runtime: lambda.Runtime.PROVIDED,
       handler: "src/Lib.handler",
       timeout: cdk.Duration.seconds(10),
-      layers: [haskellRuntime]
+      layers: [haskellRuntime],
+      environment: {"QUEUE_URL": queue.queueUrl}
     });
+
+    queue.grantSendMessages(extractNewsLambda);
   }
 }
 
